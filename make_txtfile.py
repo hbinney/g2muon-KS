e536b98cfa321503323363f2d32a8929d42807de
Hannah Binney <hannah.binney@gmail.com>
2019-03-19 18:56:08 -0500

    File to generate .tsv file used by KS.py

--- make_txtfile.py
+++ make_txtfile.py
@@ -7,29 +7,20 @@ import os.path
 from scipy import stats
 from matplotlib.backends.backend_pdf import PdfPages
 import csv
+import sys
 
-def pass_filter(t):
-    if(t.integraltubeA+t.integraltubeB > 20000 and \
-           (t.timetubeA+t.timetubeB)/2 > 20000 and \
-           (t.timetubeA+t.timetubeB)/2 < 25050 and \
-           (t.tRMStubeA+t.tRMStubeB)/2 > 20 and \
-           (t.tRMStubeA+t.tRMStubeB)/2 < 50 and \
-           t.syncPulseTimetubeA > 5300 and \
-           t.syncPulseTimetubeA < 5600 and \
-           t.syncPulseIntegraltubeA > 8000 and \
-           t.ctag > 100 and \
-           t.ctag/(t.integraltubeA+t.integraltubeB) > 0.0005):
-        return True
-    else:
-        return False
+runstr = str(sys.argv[1])
 
-with open('endgame.txt', 'r') as myfile:
-    runs=myfile.read().split('\n')
-runstr = 'endgame'
+try:
+    with open(runstr+".txt", "r") as myfile:
+        runs=myfile.read().split('\n')
+except Exception as ex:
+    template = "An exception of type {0} occurred. Arguments:\n{1!r}\nExpected Format of input file is arg.txt"
+    message = template.format(type(ex).__name__, ex.args)
+    print(message)
+    sys.exit()
 
-runs = runs[:200]
-
-with open("endgame_dqc_long.tsv", "w") as tsvfile:
+with open(runstr+".tsv", "w") as tsvfile:
     writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
     writer.writerow(["run num", "subrun num", "event num", "pulse index", "int sum", "time", "RMS", "ctag"])
     for run in runs:
@@ -41,14 +32,13 @@ with open("endgame_dqc_long.tsv", "w") as tsvfile:
                 num = t.GetEntries()
                 for i in range(0,num):
                     if(i%1000==0):
-                        print i
+                        print(i)
                     t.GetEntry(i)
-                    if(pass_filter(t)):
-                        writer.writerow([t.runNum, t.subRunNum, t.eventNum, t.pulseIndex, t.integraltubeA+t.integraltubeB, \
-                                             (t.timetubeA+t.timetubeB)/2, (t.tRMStubeA+t.tRMStubeB)/2, t.ctag])
+                    writer.writerow([t.runNum, t.subRunNum, t.eventNum, t.pulseIndex, t.integraltubeA+t.integraltubeB, \
+                                         (t.timetubeA+t.timetubeB)/2, (t.tRMStubeA+t.tRMStubeB)/2, t.ctag])
             except Exception as ex:
                 template = "An exception of type {0} occurred. Arguments:\n{1!r}"
                 message = template.format(type(ex).__name__, ex.args)
-                print message
-
-
+                print(message)
+        else:
+            print("File "+run+" does not exist\n")
